2f7e7fefb341d0a2b9f3292905fdbb4b9bfa62cb
kiddnoke <kiddnoke@163.com>
2018-11-04 07:22:16 +0800

    线上环境用PM2 做 守护进程 pm2.config.js 中几个字段可以作为替换用，作为启动传参数

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "nodemgr",
     script: "app.js",
-    args: ['-H', 'localhost'],
+    args: ['-H', 'localhost', '-S', 'State', '-A', 'Area'],
     watch: true,
   }]
 };
