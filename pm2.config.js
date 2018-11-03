000011849e557dae45df325f29addbc5215855dd
kiddnoke <kiddnoke@163.com>
2018-11-04 07:13:43 +0800

    线上环境用PM2 做 守护进程

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name        : "nodemgr",
     script      : "app.js",
-    args:['-H','10.0.2.70','-S','CN' ,'-A','1' ],
+    args:['-H','localhost','-S','CN' ,'-A','1' ],
     watch       : true,
   }]
 };
