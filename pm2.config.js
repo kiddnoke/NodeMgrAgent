a28dbf2b571c0316604c1689ac9087aa5a2afc83
zhangsen <kiddnoke>
2018-11-30 01:34:29 +0800

    PM2 web

--- pm2.config.js
+++ pm2.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   apps: [{
-    name: "nodemgr",
+    name: "nodemgr0",
     script: "app.js",
-    args: ['-H', 'localhost', '-S', 'State', '-A', 'Area'],
+    args: ['-H', '47.88.218.197', '-M', '8000', '-C', '9000', '-R', '20000:20999', '-S', 'SG', '-A', '1'],
+    watch: true,
+  }, {
+    name: "ssmanager0",
+    script: "ssmanager",
+    interpreter: "python",
+    args: ['--manager-address', '127.0.0.1:8000', '--log-file=/var/log/shadowsocks0.log', '--pid-file=/var/run/shadowsocks0.pid'],
     watch: true,
   }]
 };
